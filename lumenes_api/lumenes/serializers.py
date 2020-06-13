9008c1573a1ee93c0a7b7117557de2e4f284879a
Ángel Oreste <oresgaze@gmail.com>
2020-06-13 16:58:42 +0200

    #7: serializer and view created for Video model

--- lumenes_api/lumenes/serializers.py
+++ lumenes_api/lumenes/serializers.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from lumenes_api.lumenes.models import Video
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ('username', 'email')
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ('title', 'url', 'upload_date')
--- lumenes_api/lumenes/views.py
+++ lumenes_api/lumenes/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, permissions
-from lumenes_api.lumenes.serializers import UserSerializer
+from rest_framework import viewsets, permissions, generics
+
+from lumenes_api.lumenes.models import Video
+from lumenes_api.lumenes.serializers import UserSerializer, VideoSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAdminUser,)
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+
+class ListVideosView(generics.ListAPIView):
+    """
+    Provides a get method handler for the videos
+    """
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
