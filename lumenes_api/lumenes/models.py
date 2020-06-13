59ae0382c0dc3031b469e03372fd9b6f02f6e33a
Ángel Oreste <oresgaze@gmail.com>
2020-06-13 16:52:27 +0200

    #7: Video model updated with __str__ method

--- lumenes_api/lumenes/models.py
+++ lumenes_api/lumenes/models.py
@@ -4,6 +4,12 @@ from django.db import models
 
 
 class Video(models.Model):
-    title = models.CharField(max_length=100)
-    url = models.URLField(max_length=200)
-    upload_date = models.DateField(default=datetime.now)
+    # Title assigned to the video
+    title = models.CharField(max_length=100, null=False)
+    # URL related to the video
+    url = models.URLField(max_length=200, null=False)
+    # Date when the video was uploaded
+    upload_date = models.DateField(default=datetime.now, null=False)
+
+    def __str__(self):
+        return self.title
