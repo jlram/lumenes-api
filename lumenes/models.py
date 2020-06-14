2d7ecb1cca9805bc011325431493aa50cd2e73df
Ángel Oreste <oresgaze@gmail.com>
2020-06-14 11:07:14 +0200

    #7: simplified Video model

--- lumenes/models.py
+++ lumenes/models.py
@@ -3,16 +3,10 @@ from datetime import datetime
 from django.db import models
 
 
-BASE_URL = "https://rotula.net"
-
-
 class Video(models.Model):
 
-    # Title assigned to the video
-    title = models.CharField(max_length=100, null=False)
-    # URL related to the video
-    url = models.URLField(default=BASE_URL, max_length=200, null=False, unique=True)
-    # Date when the video was uploaded
+    title = models.CharField(max_length=100, null=False, unique=True)
+    url = models.URLField(max_length=200, null=False, unique=True)
     upload_date = models.DateField(default=datetime.now, null=False)
 
     def __str__(self):
